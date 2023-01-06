37fd7c63212e908934439f3cc216947e6d63bb2c
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-01-06 11:28:39 +0300

    Time: 57 ms (37.47%), Space: 13.8 MB (98.40%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -1,18 +1,56 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str:
-        decrypted_str = []
-        idx=0
+        ptr = len(s)-1
+        answer = []
         
-        while idx < len(s):
-            if idx+2<len(s) and s[idx+2] == "#":
-                temp = int(s[idx:idx+2])
-                temp += 96
-                decrypted_str.append(chr(temp))
-                idx+=3
+        while ptr >= 0:
+            letter = ""
+            
+            if s[ptr] == "#":
+                letter = int(s[ptr-2:ptr]) + 96
+                
+                ptr-=3
             else:
-                asci = int(s[idx]) + 96
-                decrypted_str.append(chr(asci))
-                idx+=1
+                letter = int(s[ptr]) + 96
+                
+                ptr-=1
+            
+            answer.append(chr(letter))
+        
+        answer.reverse()
+        
+        return "".join(answer)
+
+        
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         decrypted_str = []
+#         idx=0
+        
+#         while idx < len(s):
+#             if idx+2<len(s) and s[idx+2] == "#":
+#                 temp = int(s[idx:idx+2])
+#                 temp += 96
+#                 decrypted_str.append(chr(temp))
+#                 idx+=3
+#             else:
+#                 asci = int(s[idx]) + 96
+#                 decrypted_str.append(chr(asci))
+#                 idx+=1
             
         
-        return "".join(decrypted_str)
+#         return "".join(decrypted_str)
