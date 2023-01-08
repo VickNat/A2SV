15b6bbf0e20891f9f645e534fff8c573071ec6c6
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-01-08 12:35:33 +0300

    Time: 40 ms (58.48%), Space: 13.8 MB (96.58%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,19 +1,54 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        goalParser = ""
-        length = len(command)
-        index = 0
+        result = []
+        letter = 0
         
-        while index<length:
-            if command[index] == "G":
-                goalParser += "G"
-                index += 1
-            elif command[index:index+2] == "()":
-                goalParser += "o"
-                index += 2
+        while letter < len(command):
+            if command[letter] == "G":
+                result.append("G")
+                letter += 1
+            elif command[letter] == "(" and command[letter+1] == ")":
+                result.append("o")
+                letter += 2
             else:
-                goalParser += "al"
-                index += 4
+                result.append("al")
+                letter+=4
         
-        return goalParser
+        return "".join(result)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         goalParser = ""
+#         length = len(command)
+#         index = 0
+        
+#         while index<length:
+#             if command[index] == "G":
+#                 goalParser += "G"
+#                 index += 1
+#             elif command[index:index+2] == "()":
+#                 goalParser += "o"
+#                 index += 2
+#             else:
+#                 goalParser += "al"
+#                 index += 4
+        
+#         return goalParser
         
