08060cc1806a6dec94c1abaac0b867a9b78ce998
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-01-08 13:33:13 +0300

    Time: 691 ms (99.65%), Space: 19.2 MB (68.30%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
@@ -1,19 +1,59 @@
 class Solution:
     def nearestValidPoint(self, x: int, y: int, points: List[List[int]]) -> int:
-        smallest = []
-        index = -1
-        for idx in range(len(points)):
-            if points[idx][0] == x or points[idx][1] == y:
-                
-                distance = abs(x-points[idx][0]) + abs(y-points[idx][1])
-                
-                if len(smallest) == 0:
-                    smallest.append(distance)
-                    index = idx
-                
-                if distance<smallest[-1] and len(smallest)!=0:
-                    smallest.append(distance)
-                    index = idx
+        minDist = []
+        minIdx = -1
         
-        return index
+        for point in range(len(points)):
+            xIdx = points[point][0]
+            yIdx = points[point][1]
+            
+            if x == xIdx or y == yIdx:
+                distance = abs(x-xIdx) + abs(y-yIdx)
+                
+                if len(minDist) == 0:
+                    minDist.append(distance)
+                    minIdx = point
+                
+                elif minDist[-1]>distance:
+                    minDist.append(distance)
+                    minIdx = point
+                
+        return minIdx
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         smallest = []
+#         index = -1
+#         for idx in range(len(points)):
+#             if points[idx][0] == x or points[idx][1] == y:
+                
+#                 distance = abs(x-points[idx][0]) + abs(y-points[idx][1])
+                
+#                 if len(smallest) == 0:
+#                     smallest.append(distance)
+#                     index = idx
+                
+#                 if distance<smallest[-1] and len(smallest)!=0:
+#                     smallest.append(distance)
+#                     index = idx
+        
+#         return index
                 
