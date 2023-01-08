487ba49727ac6426119a22d748dc4df43a99d2d6
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-01-08 11:59:06 +0300

    Time: 32 ms (96.13%), Space: 13.9 MB (34.88%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -6,13 +6,14 @@ class Solution:
         # value = 0
         # orderList = list(order)
         
+#         We add the order to alphabets in increasing order
         for letter in order:
             alphabets[letter] = order.index(letter)
             # value+=1
             
-        idx1 = 0
+        # idx1 = 0
         
-        for idx1 in range(idx1, len(words)):
+        for idx1 in range(len(words)):
             word1 = words[idx1]
             idx2 = idx1+1
             
@@ -20,19 +21,23 @@ class Solution:
                 word2 = words[idx2]
                 letterIdx = 0
                 
+#                 While loop to compare the letters in the two words
                 while letterIdx < len(word2):
                     word1Letter = word1[letterIdx]
                     word2Letter = word2[letterIdx]
                     
                     if alphabets[word1Letter] < alphabets[word2Letter] and letterIdx+1 != len(word1):
                         break
+#                         We break the statement because we need to compare the rest of the words. If i return true here it will not compare the rest of the words in the list
                         
-                    if alphabets[word1Letter] > alphabets[word2Letter]:
+                    if (alphabets[word1Letter] > alphabets[word2Letter]) or (letterIdx+1 == len(word2) and alphabets[word1Letter] == alphabets[word2Letter] and len(word1) > len(word2)):
                         return False
                     elif letterIdx+1 == len(word1) and alphabets[word1Letter] == alphabets[word2Letter] and len(word1) <= len(word2):
                         break
-                    elif letterIdx+1 == len(word2) and alphabets[word1Letter] ==alphabets[word2Letter] and len(word1) > len(word2):
-                        return False
+#                         similar reason as the first break statement
+
+                    # elif letterIdx+1 == len(word2) and alphabets[word1Letter] == alphabets[word2Letter] and len(word1) > len(word2):
+                    #     return False
                     else:
                         letterIdx += 1
             
