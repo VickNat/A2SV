262b707e867c5d67ad9c4f7632fa34e8f72e9673
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-01-10 17:42:27 +0300

    Update TheCaptainsRoom.py

--- TheCaptainsRoom.py
+++ TheCaptainsRoom.py
@@ -1,15 +1,19 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-k_group = input()
-room_nums = list(map(int, input().split()))
-groups = set()
-captain = set()
+familySize = int(input())
 
-for idx in room_nums:
-    if idx not in groups:
-        groups.add(idx)
-        captain.add(idx)
-    else:
-        captain.discard(idx)
+roomNums = list(map(int, input().split()))
+families = dict()
+captain = 0
 
-captain = list(captain)
-print(captain[0])
+for fam in roomNums:
+    if fam not in families.keys():
+        families[fam] = 1
+    elif fam in families.keys():
+        families[fam] += 1
+
+for cap in families.keys():
+    if families[cap] == 1:
+        captain = cap
+        break
+
+print(captain)
