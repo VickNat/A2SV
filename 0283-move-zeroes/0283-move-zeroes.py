56c598520e6c6b5ee3247632392835f8ca85467b
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-01-12 22:53:01 +0300

    Time: 179 ms (72.63%), Space: 15.6 MB (61.52%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,13 +4,40 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        j = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                # temp = nums[i]
-                # nums[j] = nums[i]
-                # nums[j] = temp
-                nums[i], nums[j] = nums[j], nums[i]
+        zeroPtr = 0
+        numPtr = 0
+        
+        while numPtr < len(nums):
+            if nums[numPtr] != 0:
+                nums[zeroPtr], nums[numPtr] = nums[numPtr], nums[zeroPtr]
+                zeroPtr += 1
+                numPtr += 1
+            else:
+                numPtr += 1
                 
-                j += 1
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
+#         j = 0
+#         for i in range(len(nums)):
+#             if nums[i] != 0:
+#                 # temp = nums[i]
+#                 # nums[j] = nums[i]
+#                 # nums[j] = temp
+# #                 Swap them
+#                 nums[i], nums[j] = nums[j], nums[i]
+                
+#                 j += 1
         
