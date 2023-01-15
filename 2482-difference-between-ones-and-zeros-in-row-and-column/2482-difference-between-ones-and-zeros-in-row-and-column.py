e2b213a9ac17d09df8c242c123ff3299f8f6082e
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-01-15 21:08:39 +0300

    Time: 1905 ms (76.11%), Space: 50.6 MB (27.57%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -1,13 +1,44 @@
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
         diff = []
-        rowZeroSum = []
-        rowOneSum = []
-        colZeroSum = []
-        colOneSum = []
         gridSize = len(grid)
         colSize = len(grid[0])
+        rowZeroSum = [0]*gridSize
+        rowOneSum = [0]*gridSize
+        colZeroSum = [0]*colSize
+        colOneSum = [0]*colSize
         
+        for idx in range(gridSize):
+            for elm in range(colSize):
+                val = grid[idx][elm]
+                
+                if val == 0:
+                    rowZeroSum[idx] += 1
+                else:
+                    rowOneSum[idx] += 1
+        
+        for idx in range(colSize):
+            for elm in range(gridSize):
+                val = grid[elm][idx]
+                
+                if val == 0:
+                    colZeroSum[idx] += 1
+                else:
+                    colOneSum[idx] += 1
+        
+        for idx in range(gridSize):
+            difference = []
+
+            for elm in range(colSize):
+                difference.append((rowOneSum[idx] + colOneSum[elm]) - (rowZeroSum[idx] + colZeroSum[elm]))
+                
+            diff.append(difference)
+        
+        return diff
+        
+        
+        
+        '''
         for idx in range(gridSize):
             tempZ = 0
             tempO = 0
@@ -37,15 +68,5 @@ class Solution:
             
             colZeroSum.append(tempZ)
             colOneSum.append(tempO)
-        
-        for idx in range(gridSize):
-            difference = []
-
-            for elm in range(colSize):
-                difference.append((rowOneSum[idx] + colOneSum[elm]) - (rowZeroSum[idx] + colZeroSum[elm]))
-                
-            diff.append(difference)
-        
-        return diff
-        
+        '''
         
