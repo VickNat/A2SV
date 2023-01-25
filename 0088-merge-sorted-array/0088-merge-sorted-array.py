c60da2b2078d2da7ddf3b58c49195cd122c02465
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-01-25 10:13:57 +0300

    Time: 52 ms (36.23%), Space: 14 MB (32.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,25 +3,65 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        ptr1 = m-1
-        ptr2 = n-1
+        ptr1 = 0
+        ptr2 = 0
         
-        if ptr1==-1:
-            nums1.clear()
-            nums1.extend(nums2)
-        
-        while ptr2>=0:
-            if nums2[ptr2]>=nums1[ptr1]:
-                
-                nums1.insert((ptr1+1), nums2[ptr2])
-                ptr2-=1
+        while ptr2 < n:
+            if nums1[ptr1] == 0 and ptr1 >= m+ptr2:
+                nums1.insert(ptr1, nums2[ptr2])
+                ptr2 += 1
                 nums1.pop()
-            elif ptr1 == 0:
-                
-                nums1.insert((ptr1), nums2[ptr2])
-                ptr2-=1
-                nums1.pop()
+            elif nums1[ptr1] <= nums2[ptr2]:
+                ptr1 += 1
+                # continue
             else:
-                ptr1-=1
+                nums1.insert(ptr1, nums2[ptr2])
+                ptr2 += 1
+                nums1.pop()
+            
+            # ptr1 += 1
+        
+        return nums1
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         ptr1 = m-1
+#         ptr2 = n-1
+        
+#         if ptr1==-1:
+#             nums1.clear()
+#             nums1.extend(nums2)
+        
+#         while ptr2>=0:
+#             if nums2[ptr2]>=nums1[ptr1]:
+                
+#                 nums1.insert((ptr1+1), nums2[ptr2])
+#                 ptr2-=1
+#                 nums1.pop()
+#             elif ptr1 == 0:
+                
+#                 nums1.insert((ptr1), nums2[ptr2])
+#                 ptr2-=1
+#                 nums1.pop()
+#             else:
+#                 ptr1-=1
                 
         
