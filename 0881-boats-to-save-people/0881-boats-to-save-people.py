697a7bad36396eeb0c6548f8d0d395bc03ae4b43
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-01-25 09:26:29 +0300

    Time: 1557 ms (5.02%), Space: 20.9 MB (66.89%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -7,10 +7,10 @@ class Solution:
         people.sort()
         
         while left <= right:
-            if people[left] + people[right] > limit:
-                right -= 1
-            elif people[left] + people[right] <= limit:
+            if people[left] + people[right] <= limit:
                 left += 1
+                right -= 1
+            else:
                 right -= 1
     
             boats += 1
