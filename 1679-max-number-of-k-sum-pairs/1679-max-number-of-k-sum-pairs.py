dcb9632795abdb5ceaf79f1e064d299965bd7191
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-02-01 21:48:01 +0300

    Time: 726 ms (52.82%), Space: 26.3 MB (84.79%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -3,16 +3,15 @@ class Solution:
         nums.sort()
         left = 0
         right = len(nums) - 1
-        operations = 0
-        
+        count = 0
         while left < right:
             if nums[left] + nums[right] == k:
-                operations += 1
+                count += 1
                 left += 1
                 right -= 1
             elif nums[left] + nums[right] > k:
                 right -= 1
             else:
                 left += 1
-        
-        return operations
+                
+        return count
