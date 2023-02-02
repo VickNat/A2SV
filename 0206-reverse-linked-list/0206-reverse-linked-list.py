d697a703a3400c3be4b628e0459cec34e4a057de
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-02-02 20:14:58 +0300

    Time: 43 ms (48.43%), Space: 15.4 MB (91.32%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,42 +5,59 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        curVal = head
-        revList = []
+        pNode = None
+        curNode = head
+        nNode = None
         
-        if curVal is None or curVal.next is None:
-            return curVal
-        
-        while curVal.next is not None:
-            revList.append(curVal.val)
-            curVal = curVal.next
-        
-        revList.append(curVal.val)
-        
-        print(revList)
-                
-        right = len(revList) - 1
-        
-        head = None
-        
-        while right >= 0:
-            addIdx = ListNode(revList[right], None)
+        while curNode is not None:
+            nNode = curNode.next
+            curNode.next = pNode
             
-            if head is None:
-                head = addIdx
-                # head = head.next
-                # print(head.val)
-            else:
-                curVal = head
-                # print(2)
-                while curVal.next is not None:
-                    curVal = curVal.next
+            pNode = curNode
+            curNode = nNode
+                    
+        return pNode
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         curVal = head
+#         revList = []
+        
+#         if curVal is None or curVal.next is None:
+#             return curVal
+        
+#         while curVal.next is not None:
+#             revList.append(curVal.val)
+#             curVal = curVal.next
+        
+#         revList.append(curVal.val)
+        
+#         print(revList)
+                
+#         right = len(revList) - 1
+        
+#         head = None
+        
+#         while right >= 0:
+#             addIdx = ListNode(revList[right], None)
+            
+#             if head is None:
+#                 head = addIdx
+#             else:
+#                 curVal = head
+#                 while curVal.next is not None:
+#                     curVal = curVal.next
                     
                 
-                curVal.next = addIdx
-                # print(curVal.val)
+#                 curVal.next = addIdx
                 
-            right -= 1
+#             right -= 1
         
-        return head
+#         return head
                 
