90694c19ee9b786badbd57e3b0d419b90d78e291
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-02-03 19:58:18 +0300

    Time: 36 ms (95.67%), Space: 13.8 MB (64.75%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -5,29 +5,61 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
-            return head
-        
-        curNode = head
-        fastNode = head.next
-        delNode = None
-        
-        while curNode is not None:
+        if head:
+            leftNode = head
+            rightNode = leftNode.next
             
-            if fastNode is not None and curNode.val == fastNode.val:
-                delNode = fastNode
-                fastNode = fastNode.next
-            elif fastNode is not None and curNode.val != fastNode.val:
-                if delNode is not None:
-                    delNode.next = None
-                    curNode.next = fastNode
-                    curNode = fastNode
-                    fastNode = fastNode.next
-                elif delNode is None:
-                    curNode = fastNode
-                    fastNode = fastNode.next
-            else:
-                curNode.next = None
-                curNode = curNode.next
+            while rightNode:
+                if leftNode.val == rightNode.val:
+                    rightNode = rightNode.next
+                else:
+                    leftNode.next = rightNode
+                    
+                    leftNode = rightNode
+                    rightNode = rightNode.next
+                
+            leftNode.next = rightNode
         
         return head
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if head is None:
+#             return head
+        
+#         curNode = head
+#         fastNode = head.next
+#         delNode = None
+        
+#         while curNode is not None:
+            
+#             if fastNode is not None and curNode.val == fastNode.val:
+#                 delNode = fastNode
+#                 fastNode = fastNode.next
+#             elif fastNode is not None and curNode.val != fastNode.val:
+#                 if delNode is not None:
+#                     delNode.next = None
+#                     curNode.next = fastNode
+#                     curNode = fastNode
+#                     fastNode = fastNode.next
+#                 elif delNode is None:
+#                     curNode = fastNode
+#                     fastNode = fastNode.next
+#             else:
+#                 curNode.next = None
+#                 curNode = curNode.next
+        
+#         return head
