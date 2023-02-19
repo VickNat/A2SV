2c925ea909c5811f10a9494e2163348ced10c9a5
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-02-19 12:48:32 +0300

    Update LessOrEqual.py

--- LessOrEqual.py
+++ LessOrEqual.py
@@ -1,11 +1,11 @@
 n, k = list(map(int, input().split()))
-
+ 
 arr = sorted(list(map(int, input().split())))
-
+ 
 ans = -1
-
+ 
 if k != 0 and k < n and arr[k] != arr[k-1]:
-    for i in range(arr[k-1] + 1, 0, -1):
+    for i in range(arr[k-1] + 1, arr[k-1]-1, -1):
         if i not in arr or (i in arr and i == arr[k-1]):
             ans = i
             break
