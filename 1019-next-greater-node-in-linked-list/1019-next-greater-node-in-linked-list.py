36a05d9819f2455bdf0639fc39253edec1615531
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-02-19 20:48:03 +0300

    Time: 348 ms (27.65%), Space: 19 MB (36.45%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.py
@@ -20,26 +20,17 @@ class Solution:
         
         while head:
             val = head.val
-            
-            if len(stack) == 0:
-                stack.append(val)
-                head = head.next
-            elif stack[-1] < val:
-                temp = ptr - 1
+            temp = ptr - 1
                 
-                while len(stack) > 0 and stack[-1] < val:
-                    if ans[temp] == 0:
-                        ans[temp] = val
-                        stack.pop()
-                    temp -= 1
-                
-                stack.append(val)
-                head = head.next
+            while len(stack) > 0 and stack[-1] < val:
+                if ans[temp] == 0:
+                    ans[temp] = val
+                    stack.pop()
                     
-            elif stack[-1] >= val:
-                stack.append(val)
-                head = head.next
-            
+                temp -= 1
+                    
+            stack.append(val)
+            head = head.next
             ptr += 1
 
         return ans
