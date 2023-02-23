a0b31a4c3d246fa7774ab411022a88009deca1b8
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-02-23 09:48:03 +0300

    Time: 119 ms (71.47%), Space: 15.4 MB (26.13%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -2,37 +2,34 @@ class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         k = len(p)
         window = defaultdict(int)
-        comp = defaultdict(int)
-        ans = []
+        anagram = defaultdict(int)
+        anIdx = []
         start = 0
         end = 0
         
         if len(p) > len(s):
-            return ans
+            return anIdx
         
         for idx in range(len(p)):
             window[p[idx]] += 1        
         
-        while end < k:
-            comp[s[end]] += 1
-            end += 1
+        for end in range(len(s)):
+            anagram[s[end]] += 1
             
-        end -= 1
-        while start <= len(s) - k and end <= len(s):
-            if start != 0:
-                comp[s[end]] += 1
+            if end - start + 1 > k:
+                if anagram[s[start]] > 1:
+                    anagram[s[start]] -= 1
+                else:
+                    del anagram[s[start]]
+                    
+                start += 1
+            
+            
+            if end - start + 1 == k and window == anagram:
+                anIdx.append(start)
+            
                 
-            if comp == window:
-                ans.append(start)
-            
-            if comp[s[start]] == 1:
-                del comp[s[start]]
-            else:
-                comp[s[start]] -= 1
-            
-            end += 1
-            start += 1
-        return ans
+        return anIdx
             
             
             
