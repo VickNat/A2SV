d3a0b7df836be021cdcaaf8c8b356f03d5af44a8
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-02-23 17:39:11 +0300

    Time: 147 ms (56.28%), Space: 13.9 MB (90.68%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -14,6 +14,9 @@ class Solution:
                 repeat[s[start]] -= 1
                 start += 1
                 
+                if repeat[s[start]] == 0:
+                    repeat.pop(s[start])
+                
             maxLength = max(maxLength, end - start + 1)
         
         return maxLength
