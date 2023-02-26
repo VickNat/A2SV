ee27e9d26edab9a88a9c1e8118a71212d880b732
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-02-26 18:57:36 +0300

    Time: 237 ms (76.14%), Space: 21.3 MB (68.31%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,30 +1,17 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         size = len(nums)
-        pre = 1
-        preSumRight = [1]*(size)
-        preSumLeft = [1]*(size)
+        product = 1*nums[0]
+        prefixSum = [1]*size
         
-        for idx in range(size):
-            if idx == 0:
-                preSumRight[idx] = 1
-                continue
-            
-            preSumRight[idx] *= nums[idx - 1] * pre
-            pre *= nums[idx - 1]
+        for idx in range(1, size):
+            prefixSum[idx] *= product
+            product *= nums[idx]
+                    
+        product = 1*nums[-1]
         
-        pre = 1
+        for idx in range(size-2, -1, -1):
+            prefixSum[idx] *= product
+            product *= nums[idx]
         
-        for idx in range(size-1, -1, -1):                
-            if idx == size - 1:
-                preSumLeft[idx] = 1
-                continue
-            
-            preSumLeft[idx] *= nums[idx + 1] * pre
-            pre *= nums[idx + 1]
-        
-        for idx in range(size):
-            nums[idx] = preSumRight[idx] * preSumLeft[idx]
-        
-        return nums
-            
+        return prefixSum            
