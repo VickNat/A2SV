87e955906b2124790a7318edd6e89ad8fa7c558a
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-02-26 13:32:36 +0300

    Time: 84 ms (25.21%), Space: 14.4 MB (49.16%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,29 +1,38 @@
 class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
-        stack = []
+    def isOperator(self, operator) -> bool:
         operators = defaultdict(int)
         operators['+'] = 0
         operators['-'] = 0
         operators['*'] = 0
         operators['/'] = 0
         
+        return operator in operators
+    
+    def operation(self, a, b, elm) -> int:
+        res = 0
+        
+        if elm == '+':
+            res = int(b+a)
+        elif elm == '-':
+            res = int(b-a)
+        elif elm == '*':
+            res = int(b*a)
+        elif elm == '/':
+            res = int(b/a)
+        
+        return res
+    
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        
         for elm in tokens:
-            if elm in operators:
+            if self.isOperator(elm):
                 a = stack[-1]
                 stack.pop()
                 b = stack[-1]
                 stack.pop()
-                res = 0
                 
-                if elm == '+':
-                    res = int(b+a)
-                elif elm == '-':
-                    res = int(b-a)
-                elif elm == '*':
-                    res = int(b*a)
-                elif elm == '/':
-                    res = int(b/a)
-                
+                res = self.operation(a, b, elm)
                 stack.append(res)
             else:
                 stack.append(int(elm))
