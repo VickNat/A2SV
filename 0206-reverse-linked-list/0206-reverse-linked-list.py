fea9693783e5a83db6685cd416ad3dfb3c33fd10
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-03-01 10:33:03 +0300

    Time: 33 ms (88.02%), Space: 15.5 MB (24.85%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -9,55 +9,13 @@ class Solution:
         curNode = head
         nNode = None
         
-        while curNode is not None:
+        while curNode:
             nNode = curNode.next
             curNode.next = pNode
-            
             pNode = curNode
             curNode = nNode
                     
         return pNode
         
-        
-        
-        
-        
-        
-        
-        
-        
-#         curVal = head
-#         revList = []
-        
-#         if curVal is None or curVal.next is None:
-#             return curVal
-        
-#         while curVal.next is not None:
-#             revList.append(curVal.val)
-#             curVal = curVal.next
-        
-#         revList.append(curVal.val)
-        
-#         print(revList)
-                
-#         right = len(revList) - 1
-        
-#         head = None
-        
-#         while right >= 0:
-#             addIdx = ListNode(revList[right], None)
-            
-#             if head is None:
-#                 head = addIdx
-#             else:
-#                 curVal = head
-#                 while curVal.next is not None:
-#                     curVal = curVal.next
-                    
-                
-#                 curVal.next = addIdx
-                
-#             right -= 1
-        
-#         return head
+
                 
