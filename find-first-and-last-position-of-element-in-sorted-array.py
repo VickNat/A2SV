4ace5f4419ca6eab5c37d62fdfb3b953f7e5a03d
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-03-05 21:31:42 +0300

    added find-first-and-last-position-of-element-in-sorted-array.python

--- find-first-and-last-position-of-element-in-sorted-array.py
+++ find-first-and-last-position-of-element-in-sorted-array.py
@@ -3,34 +3,23 @@ class Solution:
         while left <= right:
             mid = (left + right)//2
 
-            if arr[mid] > target:
-                right = mid - 1
-            elif arr[mid] < target:
+            if arr[mid] <= target:
                 left = mid + 1
             else:
-                while mid < len(arr) and arr[mid] == target:
-                    mid += 1
-                
-                return mid - 1
-            
-        return -1
+                right = mid - 1
+        
+        return right
 
     def leftSearch(self, target, left, right, arr):
         while left <= right:
             mid = (left + right)//2
 
-            if arr[mid] > target:
-                right = mid - 1
-            elif arr[mid] < target:
+            if arr[mid] < target:
                 left = mid + 1
             else:
-                while mid >= 0 and arr[mid] == target:
-                    mid -= 1
-                
-                return mid + 1
-            
-        return -1
-
+                right = mid - 1
+        
+        return left
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         left = 0
         right = len(nums) - 1
@@ -38,5 +27,10 @@ class Solution:
 
         ans[0] = self.leftSearch(target, left, right, nums)
         ans[1] = self.rightSearch(target, left, right, nums)
+
+        if ans[0] >= len(nums) or nums[ans[0]] != target:
+            ans[0] = -1
+        if ans[1] < 0 or nums[ans[1]] != target:
+            ans[1] = -1
 
         return ans
