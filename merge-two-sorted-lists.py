26d94725b2c1227441a2f64527417e833b8cd3c4
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-03-12 18:24:40 +0300

    added merge-two-sorted-lists.python

--- merge-two-sorted-lists.py
+++ merge-two-sorted-lists.py
@@ -20,7 +20,7 @@ class Solution:
                 return
             
             temp = None
-
+            
             if list1.val > list2.val:
                 temp = list2
                 list2 = list2.next
@@ -28,7 +28,6 @@ class Solution:
                 temp = list1
                 list1 = list1.next
 
-            temp.next = None
             dummy.next = temp
             dummy = dummy.next
 
