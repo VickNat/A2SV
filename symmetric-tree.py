3a20715e0dbea55ca7f615bbeb80ba236bd762a3
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-03-14 07:37:44 +0300

    added symmetric-tree.python

--- symmetric-tree.py
+++ symmetric-tree.py
@@ -5,32 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def invert(self, root):
-        if not root:
-            return root
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
 
-        root.left, root.right = root.right, root.left
+        def check(rootL, rootR):
+            if not rootL and not rootR:
+                return True
+            if not rootL or not rootR:
+                return False
+            
+            left = check(rootL.left, rootR.right)
+            right = check(rootL.right, rootR.left)
 
-        left = self.invert(root.left)
-        right = self.invert(root.right)
-
-        return root
-
-    def check(self, root1, root2):
-        if not root1 and not root2:
-            return True
-        elif (root1 and not root2) or (root2 and not root1):
+            if left and right:
+                if rootL.val == rootR.val:
+                    return True
+            
             return False
         
-        left = self.check(root1.left, root2.left)
-        right = self.check(root1.right, root2.right)
-
-        if left and right and root1.val == root2.val:
-            return True
-        return False
-
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        inverted = self.invert(root.right)
-        ans = self.check(root.left, inverted)
-        
-        return ans
+        return check(root.left, root.right)
