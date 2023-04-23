b91a40b5f5f1914f872f239351c29000cb1dfb5a
NatiVick <114672453+VickNat@users.noreply.github.com>
2023-04-23 17:59:25 +0300

    added sum-of-nodes-with-even-valued-grandparent.python

--- sum-of-nodes-with-even-valued-grandparent.py
+++ sum-of-nodes-with-even-valued-grandparent.py
@@ -5,34 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.sums = 0
-        self.counts = 0
-
+    
     def sumEvenGrandparent(self, root: TreeNode) -> int:
-        def sumGrandChildren(root):
-            self.counts += 1
-            if not root:
-                self.counts -= 1
+        sum_ = 0
+
+        def dfs(parent, gParent, node):
+            nonlocal sum_
+
+            if not node:
                 return
 
-            if self.counts == 3:
-                self.sums += root.val
-                self.counts -= 1
-                return
+            if gParent > 0 and gParent%2 == 0:
+                sum_ += node.val
 
-            left = sumGrandChildren(root.left)
-            right = sumGrandChildren(root.right)
-            self.counts -= 1
-            return
-
-        if not root:
-            return
+            dfs(node.val, parent, node.left)
+            dfs(node.val, parent, node.right)
         
-        if root.val%2 == 0:
-            sumGrandChildren(root)
-        
-        left = self.sumEvenGrandparent(root.left)
-        right = self.sumEvenGrandparent(root.right)
-
-        return self.sums
+        dfs(0, 0, root)
+        return sum_
