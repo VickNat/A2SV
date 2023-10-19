b5e84a3cccdb94de9e1bb564fed8a40b17ac160f
Natanim Ashenafi <114672453+VickNat@users.noreply.github.com>
2023-10-19 10:05:11 +0300

    added partition-equal-subset-sum.python

--- partition-equal-subset-sum.py
+++ partition-equal-subset-sum.py
@@ -1,6 +1,5 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        memo = defaultdict(int)
         size = len(nums)
         total = sum(nums)
         nums.sort()
@@ -8,21 +7,18 @@ class Solution:
         if total%2 != 0:
             return False
 
+        @cache
         def dp(idx, sum_):
             if idx == size:
                 if sum_ == 0:
                     return True
                 return False
-            
-            if (idx, sum_) in memo:
-                return memo[(idx, sum_)]
 
             first = dp(idx+1, sum_+nums[idx])
             second = False
             if not first:
                 second = dp(idx+1, sum_-nums[idx])
 
-            memo[(idx, sum_)] = first or second
-            return memo[(idx, sum_)]
+            return first or second
         
         return dp(0, 0)
